625731045273022c3ee1b23559c04e518da1a1b3
Brian Palmer <brian@codekitchen.net>
2018-02-20 21:42:22 -0700

    remove some dead code

--- quad_controller_rl/src/quad_controller_rl/tasks/hover.py
+++ quad_controller_rl/src/quad_controller_rl/tasks/hover.py
@@ -4,7 +4,6 @@ import numpy as np
 from gym import spaces
 from geometry_msgs.msg import Vector3, Point, Quaternion, Pose, Twist, Wrench
 from quad_controller_rl.tasks.base_task import BaseTask
-from quad_controller_rl.tasks.temporal_space import TemporalState
 
 class Hover(BaseTask):
     """Hover around a given target height, randomly generated for each episode."""
--- quad_controller_rl/src/quad_controller_rl/tasks/land.py
+++ quad_controller_rl/src/quad_controller_rl/tasks/land.py
@@ -6,7 +6,6 @@ import numpy as np
 from gym import spaces
 from geometry_msgs.msg import Vector3, Point, Quaternion, Pose, Twist, Wrench
 from quad_controller_rl.tasks.base_task import BaseTask
-from quad_controller_rl.tasks.temporal_space import TemporalState
 
 class Land(BaseTask):
     """Land gently."""
